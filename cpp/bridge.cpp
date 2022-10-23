884ba5591b095ecd19bf3cd12fa0c9578afe1fd1
D <25824939+ghasimi@users.noreply.github.com>
2022-10-22 20:17:25 -0400

    Create bridge.cpp

---
+++ cpp/bridge.cpp
@@ -0,0 +1,74 @@
+/*
+Design Patterns: Bridge 
+2022, D. Ghasimi
+*/
+
+#include <iostream>
+
+void print(std::string str) {std::cout << str << std::endl;};
+
+// Implementer
+class Shape 
+{
+public:
+    virtual std::string draw() {return "Shape";};
+};
+
+// Concrete Implementer
+class Circle: public Shape 
+{
+public:
+    std::string draw() {return "Circle";};
+};
+
+// Concrete Implementer
+class Rectangle : public Shape 
+{
+public:
+    std::string draw() {return "Rectangle";};
+};
+
+// Abstract
+class Display
+{
+protected:
+    Shape* _shapePointer;
+public:
+    Display(Shape* shapePointer): _shapePointer(shapePointer){};
+    virtual ~Display(){
+        delete _shapePointer; 
+        print("Display destructor deleted _shapePointer");
+    };
+    virtual std::string shapeDrawer() const = 0;
+};
+
+// Refined Abstract
+class ColorDisplay : public Display
+{
+public:
+    ColorDisplay(Shape* shapePointer) : Display(shapePointer){};
+    std::string shapeDrawer() const 
+    {
+        return "ColorDisplay " + _shapePointer->draw();
+    };
+};
+
+// Client
+void client(Display& display)
+{
+    print("Display draws a " + display.shapeDrawer());
+};
+
+int main() {
+    
+    // Implementer: A base class pointer that points to a Concrete Implementer
+    Shape* shapePointer = new Circle();
+    
+    // Refined Abstract class
+    ColorDisplay colorDisplay(shapePointer);
+    
+    // Client
+    client(colorDisplay);
+    
+    return 0;
+}
